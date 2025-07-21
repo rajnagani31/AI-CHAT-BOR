d1818ad4da23fdd32b90edf4dac5075d3f71ee57
rajnagani <rajnagani331@gmail.com>
2025-07-21 22:04:19 +0530

    change

--- bot.py
+++ bot.py
@@ -2,36 +2,44 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 from system_prompt import SYSTEM_PROMPT
+from google import genai
+from google.genai.types import GenerateContentConfig, HttpOptions
 # from chat_bot import query
 
 load_dotenv()
+client = genai.Client()
+
 # api_key=st.secrets["GEMINI_API_KEY"]
 # api_key=str(GEMINI_API_KEY)
-client=OpenAI(
-    api_key=os.getenv("GEMINI_API_KEY"),
-    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
-)
+# client=OpenAI(
+#     api_key=os.getenv("GEMINI_API_KEY"),
+#     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+# )
 # client=OpenAI()
 # openai.api_key = os.environ['OPENAI_API_KEY']
-SYSTEM_PROMPT=SYSTEM_PROMPT
+data=SYSTEM_PROMPT
 # ans=[]
 def bot_call(query):
 
         # print("query",query)
-        response=client.chat.completions.create(
-            model="gemini-1.5-flash",
-            # model='gpt-4.1-nano',
-            messages=[
-                {'role':'system','content':SYSTEM_PROMPT},
-                {'role':'user','content':query}
-                # {'role':'user','content':'hi'}
 
-                ]
-        )
 
-        msg=response.choices[0].message.content
-        # print("MSG",msg)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        # system_instruction = SYSTEM_PROMPT,
+        contents=query,
+        config=GenerateContentConfig(
+        system_instruction=[
+            data
+        ]
+    ),
+
+    )
+
+    msg=response.text
+    print("MSG",msg)
         # ans.append({'role':'assistant','content':msg})
     # return ({'role':'assistant','content':msg})
-        return msg
+    return msg
 
+# bot_call("What is your name")  # Example call to test the function
